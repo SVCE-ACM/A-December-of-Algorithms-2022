050f23efb8ef769975511d61678e447828be5ebb
SIVADHAS S <84082104+GREEDnim@users.noreply.github.com>
2022-12-01 18:48:55 -0800

    day-2 completed

---
+++ December - 02/GREEDnim_day2_java.java
@@ -0,0 +1,15 @@
+package acm;
+
+import java.util.Scanner;
+
+public class GREEDnim_day2_java {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String input =in.next();
+
+        int indexOfFirstLetter=input.length()-4;
+        String ans=input.substring(indexOfFirstLetter,indexOfFirstLetter+2)+input.substring(0,indexOfFirstLetter);
+        System.out.println(ans);
+
+    }
+}
