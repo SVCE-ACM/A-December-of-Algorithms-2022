c6f15ad96f0e94679040017adf800bb9ca4995be
Dhruv Rawat <89683914+yesdhruv@users.noreply.github.com>
2022-12-12 18:34:21 +0000

    added dec 9 problem

---
+++ December - 09/C++_Dhruv.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;cin>>n;
+    vector<string> input(n) ;
+    
+    for(auto &x:input)cin>>x;
+    int max_len=INT_MIN;
+    
+    string word;
+    for(auto e:input){
+        int len = e.size();
+        if(size(e)%2!=0)
+            {
+                max_len = max(len,max_len);
+                
+            }
+        else 
+        continue;
+    }
+    for(auto e:input ){
+        if(size(e)==max_len){
+            word = e;
+            break;
+            
+        }
+    }
+    if(max_len==INT_MIN)
+    cout<<"Better Luck Next Time";
+    else
+    cout<<word;
+    
+	return 0;
+}
