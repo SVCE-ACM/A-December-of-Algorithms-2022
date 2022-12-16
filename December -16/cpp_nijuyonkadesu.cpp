7e8308c59298fd54aef8013d189a32cbf9ed3b9b
nijuyonkadesu <85601344+nijuyonkadesu@users.noreply.github.com>
2022-12-16 19:33:08 +0530

    fix another flaw in logic

--- December -16/cpp_nijuyonkadesu.cpp
+++ December -16/cpp_nijuyonkadesu.cpp
@@ -20,16 +20,21 @@ int main(){
 int maxPossibleVolume(vector<int>& water, int n){
     int s=0, e=n-1; // pointer to start and end of the given data
     int volume = 0;
+    int currentVolume = 0;
 
-    for(int f=0, r=e; f<r; f++, r--){
+    for(int f=0, r=e; f<n && r>=0; f++, r--){
         if(water[s]<water[f]){
             s=f;
         }
         if(water[e]<water[r]){
             e=r;
         }
+        currentVolume = min(water[s],water[e]) * abs(s-e); // calculate volume
+        if(volume < currentVolume) { // track max possible volume
+            volume = currentVolume;
+        }
     }
-    return min(water[s],water[e]) * abs(s-e); // calculate volume and return it
+    return volume;
 }
 
 vector<int> input(int n){
