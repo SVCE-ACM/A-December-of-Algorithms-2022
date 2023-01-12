e38e72eb1420e4b14cfe66e20a4cf872aab145a3
MugundhanY <122536258+MugundhanY@users.noreply.github.com>
2023-01-12 18:32:06 +0530

    added December 7 - Word Map

---
+++ December - 07/C++_MugundhanY.cpp
@@ -0,0 +1,53 @@
+//Question 7:
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int x[] = { -1,  0, 0, 1};
+int y[] = {  0, -1, 1, 0};
+ 
+bool search2D(char *grid, int row, int col, string word, int R, int C){
+    if (*(grid+row*C+col) != word[0])
+        return false;
+ 
+    int len = word.length();
+ 
+    for (int dir = 0; dir < 4; dir++) {
+        int k, rd = row + x[dir], cd = col + y[dir];
+
+        for (k = 1; k < len; k++) {
+            if (rd >= R || rd < 0 || cd >= C || cd < 0)
+                break;
+                
+            if (*(grid+rd*C+cd) != word[k])
+                break;
+
+            rd += x[dir], cd += y[dir];
+        }
+ 
+
+        if (k == len)
+           return true;
+    }
+    return false;
+}
+ 
+
+void patternSearch(char *grid, string word, int R, int C){
+    for (int row = 0; row < R; row++)
+        for (int col = 0; col < C; col++)
+           if(search2D(grid, row, col, word, R, C)){
+             cout << "Found";
+             return;
+           }
+           cout << "Not Found";
+}
+ 
+int main(){
+    int R = 15, C = 15;
+    char grid[R][C] = {"ASSERTIVENESSLJ","CFOGOODPOSITIVE","OPENBMUREWOPRPS","MEDIATIONELDIOG", "MAASREGJEWINWIN", "UCIAEMOEECSKENI", "NEMRSHACDVTWTTL", "ITEHOTLTSTERAOE", "CASPLGLSIUNERFE", "AISAUSYTPOISEVF", "TTARTTDOCENPPIH", "IOGAIGUPYMGOOEE", "OGEPOAFPQIENOWA", "NECONFLICTSDCER", "FNHTCATNOCEYEBT"};
+    
+    string word;
+    cin >> word;
+    patternSearch((char *)grid, word, R, C);
+    return 0;
+}
