a137df194b070fdb0af76e801694e5633a3ea281
Samriddh Prasad <72660273+Samriddh2703@users.noreply.github.com>
2023-01-14 23:00:26 +0530

    Create day22.cpp

---
+++ December - 22/day22.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void TheMarkowitzParadox(char a, char b){
+    int c = a - 'A';
+    int e = ((int(b) + c)-65)%26;
+    char d = 65 + e;
+    cout << d;
+ }
+int main(){
+    string a, b;
+    cin >> a;
+    cin >> b;
+    char* a1 = &a[0];
+    char* b1 = &b[0];
+    int n1 = a.size(), n2 = b.size(), x=0, i=0;
+    while(i<n2){
+        if(x==n1) x = 0;
+        TheMarkowitzParadox(a1[x], b1[i]);
+        i++, x++;
+    }
+    return 0;
+}
